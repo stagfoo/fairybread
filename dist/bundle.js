0f1c5a63c03c81f6b01392aa193f50965ffd0ce0
alexander <alexanderddking@gmail.com>
2017-02-23 06:57:27 +0900

    New Github folder docs

--- dist/bundle.js
+++ dist/bundle.js
@@ -72,130 +72,59 @@
 
 "use strict";
 
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Fairybread = function () {
-    function Fairybread() {
-        _classCallCheck(this, Fairybread);
-
-        this.id = this.makeId();
-        this.masterClass = "." + this.id;
-        this.sheet = false;
-        this.specialSheet = false;
-        this.specialId = this.makeId() + "_special";
-        this.rules;
-        this.index = 0;
-        this.specialIndex = 0;
-        this.global = false;
+function _toConsumableArray(a) {
+  if (Array.isArray(a)) {
+    for (var b = 0, c = Array(a.length); b < a.length; b++) {
+      c[b] = a[b];
+    }return c;
+  }return Array.from(a);
+}function _classCallCheck(a, b) {
+  if (!(a instanceof b)) throw new TypeError("Cannot call a class as a function");
+}Object.defineProperty(exports, "__esModule", { value: !0 });var _createClass = function () {
+  function a(a, b) {
+    for (var c = 0; c < b.length; c++) {
+      var d = b[c];d.enumerable = d.enumerable || !1, d.configurable = !0, "value" in d && (d.writable = !0), Object.defineProperty(a, d.key, d);
     }
-
-    _createClass(Fairybread, [{
-        key: "makeId",
-        value: function makeId() {
-            var text = "fairybread_";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            [].concat(_toConsumableArray(Array(20))).map(function (data, key) {
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
-            });
-            return text;
-        }
-    }, {
-        key: "getAll",
-        value: function getAll() {
-            var _this = this;
-
-            var rules = this.sheet.cssRules || this.sheet.rules || {};
-            var results = {};
-            Object.keys(rules).map(function (key) {
-                var className = rules[key].selectorText.replace(_this.masterClass + " ", '');
-                var cssText = rules[key].cssText.slice(rules[key].cssText.indexOf('{') + 1).split(';');
-                var ruleSet = {};
-                cssText.map(function (data, key) {
-                    var keyValue = data.split(':');
-                    if (keyValue.length === 2) {
-                        ruleSet[keyValue[0].trim()] = keyValue[1].trim();
-                    }
-                });
-                results[className] = ruleSet;
-            });
-            return results;
-        }
-    }, {
-        key: "extend",
-        value: function extend(selector) {
-            var all = this.getAll();
-            return all[selector];
-        }
-    }, {
-        key: "add",
-        value: function add(selector, rules) {
-
-            this.sheet.insertRule ? this.sheet.insertRule(this.masterClass + " " + selector + " { " + rules + " }", this.index) : this.sheet.addRule(this.masterClass + " " + selector, rules, this.index);
-
-            this.index++;
-        }
-    }, {
-        key: "addSpecial",
-        value: function addSpecial(rule) {
-            var id = this.specialId;
-            if (this.specialSheet === false) {
-                var styleNode = document.createElement('style');
-                styleNode.type = 'text/css';
-                styleNode.id = id;
-                styleNode.rel = 'stylesheet';
-                document.head.appendChild(styleNode);
-                this.specialSheet = document.getElementById(id); //FIXME
-                this.specialSheet.innerHTML = rule;
-            } else {
-                console.log(this.specialSheet);
-                this.specialSheet.innerHTML += "\n" + rule;
-            }
-        }
-    }, {
-        key: "createScope",
-        value: function createScope() {
-            if (this.sheet === false) {
-                var styleNode = document.createElement('style');
-                styleNode.type = 'text/css';
-                styleNode.id = this.id;
-                styleNode.rel = 'stylesheet';
-                document.head.appendChild(styleNode);
-                this.sheet = styleNode.sheet;
-            } else {
-                console.error('You have already made a sheet on this instance');
-            }
-            return this.id;
-        }
-    }, {
-        key: "createGlobal",
-        value: function createGlobal() {
-            if (this.sheet === false) {
-                this.masterClass = " ";
-                var styleNode = document.createElement('style');
-                styleNode.type = 'text/css';
-                styleNode.id = this.id;
-                styleNode.rel = 'stylesheet';
-                document.head.appendChild(styleNode);
-                this.sheet = styleNode.sheet;
-            } else {
-                console.error('You have already made a sheet on this instance');
-            }
-        }
-    }]);
-
-    return Fairybread;
-}();
-
-exports.default = Fairybread;
+  }return function (b, c, d) {
+    return c && a(b.prototype, c), d && a(b, d), b;
+  };
+}(),
+    Fairybread = function () {
+  function a() {
+    _classCallCheck(this, a), this.id = this.makeId(), this.masterClass = "." + this.id, this.sheet = !1, this.specialSheet = !1, this.specialId = this.makeId() + "_special", this.rules, this.index = 0, this.specialIndex = 0, this.global = !1;
+  }return _createClass(a, [{ key: "makeId", value: function value() {
+      var b = "fairybread_",
+          c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";return [].concat(_toConsumableArray(Array(20))).map(function (a, d) {
+        b += c.charAt(Math.floor(Math.random() * c.length));
+      }), b;
+    } }, { key: "getAll", value: function value() {
+      var b = this,
+          c = this.sheet.cssRules || this.sheet.rules || {},
+          d = {};return Object.keys(c).map(function (a) {
+        var e = c[a].selectorText.replace(b.masterClass + " ", ""),
+            f = c[a].cssText.slice(c[a].cssText.indexOf("{") + 1).split(";"),
+            g = {};f.map(function (a, b) {
+          var c = a.split(":");2 === c.length && (g[c[0].trim()] = c[1].trim());
+        }), d[e] = g;
+      }), d;
+    } }, { key: "extend", value: function value(b) {
+      var c = this.getAll();return c[b];
+    } }, { key: "add", value: function value(b, c) {
+      this.sheet.insertRule ? this.sheet.insertRule(this.masterClass + " " + b + " { " + c + " }", this.index) : this.sheet.addRule(this.masterClass + " " + b, c, this.index), this.index++;
+    } }, { key: "addSpecial", value: function value(b) {
+      var c = this.specialId;if (this.specialSheet === !1) {
+        var d = document.createElement("style");d.type = "text/css", d.id = c, d.rel = "stylesheet", document.head.appendChild(d), this.specialSheet = document.getElementById(c), this.specialSheet.innerHTML = b;
+      } else console.log(this.specialSheet), this.specialSheet.innerHTML += "\n" + b;
+    } }, { key: "createScope", value: function value() {
+      if (this.sheet === !1) {
+        var b = document.createElement("style");b.type = "text/css", b.id = this.id, b.rel = "stylesheet", document.head.appendChild(b), this.sheet = b.sheet;
+      } else console.error("You have already made a sheet on this instance");return this.id;
+    } }, { key: "createGlobal", value: function value() {
+      if (this.sheet === !1) {
+        this.masterClass = " ";var b = document.createElement("style");b.type = "text/css", b.id = this.id, b.rel = "stylesheet", document.head.appendChild(b), this.sheet = b.sheet;
+      } else console.error("You have already made a sheet on this instance");
+    } }]), a;
+}();exports.default = Fairybread;
 
 /***/ }),
 /* 1 */
@@ -227,17 +156,20 @@ globalSheet.add('body', 'background:' + vars.colors.yellow);
 
 var sheet = new _fairybread2.default();
 var id = sheet.createScope();
-// sheet.createGlobal();
 
 sheet.add('a', 'color:red;');
+sheet.add('a', 'color:blue;');
+
 sheet.add('a:hover', 'color:green;');
 sheet.add('h1', 'font-family:"Permanent Marker"');
 sheet.add('p', 'color:blue; font-family: \'Roboto\';');
 sheet.add('#main', '\n              background:#efefef;\n              font-weight:bold;\n              width:900px;\n              margin:0 auto;\n              padding:1em;\n              ');
 sheet.addSpecial(' @font-face {\n    font-family: \'Permanent Marker\';\n    font-style: normal;\n    font-weight: 400;\n    src: local(\'Permanent Marker\'), local(\'PermanentMarker\'), url(https://fonts.gstatic.com/s/permanentmarker/v5/9vYsg5VgPHKK8SXYbf3sMio-5Z6V1O0VBgfXWFfbB4c.woff2) format(\'woff2\');\n    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;\n};');
 sheet.addSpecial('\n@font-face {\n  font-family: \'Roboto\';\n  font-style: normal;\n  font-weight: 400;\n  src: local(\'Roboto\'), local(\'Roboto-Regular\'), url(https://fonts.gstatic.com/s/roboto/v15/ek4gzZ-GeXAPcSbHtCeQI_esZW2xOQ-xsNqO47m55DA.woff2) format(\'woff2\');\n  unicode-range: U+0460-052F, U+20B4, U+2DE0-2DFF, U+A640-A69F;\n};');
-console.log(sheet.extend('p'));
+console.log(globalSheet.extend('body'));
 console.log(sheet.getAll());
+var tag_color = sheet.getAll()['a'];
+sheet.add('.button', 'color:' + tag_color);
 
 // console.log(id);
 document.querySelector('#main').className = id;
