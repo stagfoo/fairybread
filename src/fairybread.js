ab10131df8da1a6d3ec3d4090ae6d55ef1e84b10
Alex King <alexanderddking@gmail.com>
2018-02-06 06:05:45 +0900

    Create new render method

--- src/fairybread.js
+++ src/fairybread.js
@@ -1,49 +1,49 @@
 function Fairybread(sheetType) {
-    this.sheetType = sheetType;
-    this.scopeClass = '';
-    this.ensureList = {};
-    // Create Id
-    function makeId() {
-        var text = "fairybread_";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var array = Array.apply(null, Array(20));
-        array.map(function (data, key) { text += possible.charAt(Math.floor(Math.random() * possible.length)); });
-        return text;
-    };
-    // Uniquish Id
-    this.id = makeId();
-    // Create Sheetsheet
-    this.createSheet = function (id) {
-        var styleNode = document.createElement('style');
-        styleNode.type = 'text/css';
-        styleNode.id = id;
-        styleNode.rel = 'stylesheet';
-        // required for sheet attr to be created
-        return styleNode;
-    }
-    this.bindSheet = function (node,location) {
-        document[location].appendChild(node);
-    }
-    // Create Js Object from Css text
-    this.cssToJs = function (css) {
-        var rules = css.split(';');
-        var ruleSet = {}
-        rules.map(function (data, key) {
-            var keyValue = data.split(':');
-            if (keyValue.length === 2) {
-                ruleSet[keyValue[0].trim().toString()] = keyValue[1].trim();
-            }
-        });
-        return ruleSet;
-    }
+  this.sheetType = sheetType;
+  this.scopeClass = '';
+  this.ensureList = {};
+  // Create Id
+  function makeId() {
+    var text = "fairybread_";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var array = Array.apply(null, Array(20));
+    array.map(function (data, key) { text += possible.charAt(Math.floor(Math.random() * possible.length)); });
+    return text;
+  };
+  // Uniquish Id
+  this.id = makeId();
+  // Create Sheetsheet
+  this.createSheet = function (id) {
+    var styleNode = document.createElement('style');
+    styleNode.type = 'text/css';
+    styleNode.id = id;
+    styleNode.rel = 'stylesheet';
+    // required for sheet attr to be created
+    return styleNode;
+  }
+  this.bindSheet = function (node,location) {
+    document[location].appendChild(node);
+  }
+  // Create Js Object from Css text
+  this.cssToJs = function (css) {
+    var rules = css.split(';');
+    var ruleSet = {}
+    rules.map(function (data, key) {
+      var keyValue = data.split(':');
+      if (keyValue.length === 2) {
+        ruleSet[keyValue[0].trim().toString()] = keyValue[1].trim();
+      }
+    });
+    return ruleSet;
+  }
 
-    this.sheet = this.createSheet(this.id);
-    this.specialSheet = false;
-    this.specialId = makeId() + "_special";
-    this.rendered = false;
-    this.rules = {};
-    this.index = 0;
-    this.specialIndex = 0;
+  this.sheet = this.createSheet(this.id);
+  this.specialSheet = false;
+  this.specialId = makeId() + "_special";
+  this.rendered = false;
+  this.rules = {};
+  this.index = 0;
+  this.specialIndex = 0;
 
 }
 
@@ -92,6 +92,14 @@ Fairybread.prototype.render = function (location) {
                 css: flatSheet
             };
             break;
+        case 'here':
+          thisSheet.innerHTML = renderFlat();
+          result = {
+            tag: this.sheet,
+            id: this.sheet.id
+          }
+          break;
+
         case 'body':
             bindSheet(thisSheet, 'body');
             thisSheet.innerHTML = renderFlat();
--- tests/createLocalSheet.test.js
+++ tests/createLocalSheet.test.js
@@ -1,52 +1,52 @@
-const Fairybread = require('../src/fairybread')
-const sheet = new Fairybread('local')
+const Fairybread = require('../src/fairybread');
+
+const sheet = new Fairybread('local');
 const idTest = [
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
-  new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local'),
+	new Fairybread('local')
 ];
 
 test(`Is Sheet's defined`, () => {
-  expect(sheet).toBeDefined()
-})
+	expect(sheet).toBeDefined();
+});
 
-test(`Is it a global`,() => {
-   expect(sheet.sheetType).toBe('local')
-  expect(sheet.specialSheet).toBeFalsy()
-})
+test(`Is it a global`, () => {
+	expect(sheet.sheetType).toBe('local');
+	expect(sheet.specialSheet).toBeFalsy();
+});
 
-test(`Was a sheet created?`,() =>{
-  expect(sheet.sheet).toBeDefined()
-})
+test(`Was a sheet created?`, () => {
+	expect(sheet.sheet).toBeDefined();
+});
 
-test(`Does it have an Id?`,() =>{
-  expect(sheet.id).toBeDefined()
-})
+test(`Does it have an Id?`, () => {
+	expect(sheet.id).toBeDefined();
+});
 
-test(`Does it have different Id?`,() =>{
-  const masterId = sheet.id;
-  idTest.map((slaveSheet) => {
-   expect(masterId).not.toBe(slaveSheet.id);
-  })
-})
+test(`Does it have different Id?`, () => {
+	const masterId = sheet.id;
+	idTest.map(slaveSheet => {
+		expect(masterId).not.toBe(slaveSheet.id);
+	});
+});
 
-test(`Is it rendered?`,() =>{
-  expect(sheet.rendered).toBeFalsy()
-})
+test(`Is it rendered?`, () => {
+	expect(sheet.rendered).toBeFalsy();
+});
 
-test(`Are there no Rules yet?`,() =>{
-  expect(sheet.index).toBe(0)
-  expect(sheet.specialIndex).toBe(0)
-  expect(Object.keys(sheet.rules).length).toBe(0)
-})
-
+test(`Are there no Rules yet?`, () => {
+	expect(sheet.index).toBe(0);
+	expect(sheet.specialIndex).toBe(0);
+	expect(Object.keys(sheet.rules).length).toBe(0);
+});
 
